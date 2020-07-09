f72f607c8d9a6707a74b9378b551641dafa25de2
ferrisbueller66 <github email address>
2020-07-09 07:09:58 -0400

    create visit model

---
+++ db/migrate/20200709110918_create_visits.rb
@@ -0,0 +1,11 @@
+class CreateVisits < ActiveRecord::Migration[6.0]
+  def change
+    create_table :visits do |t|
+      t.date :date
+      t.boolean :completed
+      t.string :food_pantry
+
+      t.timestamps
+    end
+  end
+end
